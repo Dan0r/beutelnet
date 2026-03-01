05400fd873c315a3854af2bebf50d1b456a6c57c
furukawa <moshimoshi>
2026-03-01 21:26:43 +0100

    move data outside of src

--- __main__.py
+++ __main__.py
@@ -2,13 +2,22 @@ from src.images.image_pre_processor import PreProcessor
 from src.ocr.process_image import ProcessImage
 from src.ocr.process_ocr import ProcessOcr
 
-"""Where to load images from and where to place the processed images"""
-image_directory =  "/home/furukawa/programming/staub/src/images/original/"
-target_directory = "/home/furukawa/programming/staub/src/images/preprocessed/"
+"""Specify directories to load and place images"""
+image_directory =  "storage/original/"
+target_directory = "storage/preprocessed/"
+
 
 def pre_process_images():
     image_processor = PreProcessor(image_directory, target_directory)
     image_processor.preprocess()
+
+def output_ocrtext():
+    ocr_processor = ProcessImage(target_directory)
+    ocrtext = ocr_processor.scan_dir()
+    return ocrtext
+
+def commit_to_database():
+    return 1 
 
 if __name__ == '__main__':
     pre_process_images()
