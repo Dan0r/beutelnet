86f8ffcd90dcb745b9a3f5af5a911f5775bf36d3
furukawa <moshimoshi>
2026-03-02 22:24:05 +0100

    added template

--- beutelnet/bagsearch/views.py
+++ beutelnet/bagsearch/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
-def index(request):
+def index():
     return HttpResponse("EDEKA")
+
+def bag_view(request):
+    return render(request, "bagsearch/index.html")
